e47e1f30f6b1973d4599e4868701a9dc31ad9553
kulisarawiangin <kulisara.w@ku.th>
2023-02-08 16:56:18 +0700

    change home.js

--- src/services/Bulbs.js
+++ src/services/Bulbs.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 
-// const URL = "http://group15.exceed19.online/bulb/"
 
 export async function getBulbs() {
     try {
@@ -17,7 +16,9 @@ export async function updateBulb(bulb_id, body) {
     const res = await axios.put(`http://group15.exceed19.online/bulb/${bulb_id}`, body)
     return res.data
 }
-// export async function getBulb(id) {
-//     const res = await axios.get(`${URL}/${id}`)
-//     return res.data
-// }
+
+export async function getParty() {
+    // body : {"is_on": true}
+    const res = await axios.put(`http://group15.exceed19.online/party`)
+    return res.data
+}
