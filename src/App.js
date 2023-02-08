2631350ec325cb71ac2ea81ad3d0a8af2581f91c
breezjirasak <jirasak.tu@ku.th>
2023-02-08 14:28:57 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import ToggleSwitch from './components/Button';
-import './styles/ToggleSwitch.css';
-import Slidebar from './components/SlideBar';
+import Home from './pages/Home';
 
 
 function App() {
-  
-  const [Switch, setSwitch] = useState(false);
 
-  const onSwitchChange = (checked) => {
-    setSwitch(checked);
-    console.log(checked)
-  }
-  const [Scale, setScale] = useState(0);
-
-  const onScale = event => {
-    setScale(event.target.value);
-    console.log(event.target.value)
-  }
-  // const handleChange = event => {
-  //   setScale(event.target.value);
-  //   console.log(event.target.value)
-  // };
-
-  
-
-
-
-  return (
-    <>
-      <ToggleSwitch id="switch" checked={ Switch } onChange={ onSwitchChange } 
-      />
-      <label htmlFor="switch">Manual</label>
-      <Slidebar id="slide" value={ Scale } onChange={onScale}/>
-      <label htmlFor="slide">radiance</label>
-       {/* <input
-        type="number"
-        placeholder="Your fav number"
-        value={Scale}
-        onChange={handleChange}
-      /> */}
-
-    </>
-  );
+return (
+  <>
+  <Home/>
+  </>
+)
 }
 
-export default App;
+export default App
