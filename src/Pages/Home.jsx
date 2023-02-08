80cca90c042eb727c0f7231f896f45a1a67cacfb
breezjirasak <jirasak.tu@ku.th>
2023-02-08 18:30:18 +0700

    get party

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react"
 // import { getBlubs } from '../services/Bulbs'
 import Card from '../components/Card'
 import '../styles/Home.css'
-import { getBulbs, updateParty } from '../services/Bulbs'
+import { getBulbs, getParty, updateParty } from '../services/Bulbs'
 import Switch from "@mui/material/Switch";
 
 const Home = () => {
@@ -15,10 +15,12 @@ const Home = () => {
     
     useEffect(() => {
             getBulbs().then(data => setBulb(data))
+            getParty().then(data => setParty(data))
         },[])
+
+
     const onPartyChange = (event) => {
         setParty(event.target.checked);
-        console.log(event)
         updateParty({"party": event.target.checked})
     };
     
@@ -28,7 +30,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <h1 className="Light-controller">Light Controller</h1>
-            <h4 className="party">Party: <Switch {...label} checked={Party} onChange={onPartyChange} /></h4>
+            <h4 className="party">Party: <Switch {...label} checked={Party.party} onChange={onPartyChange} /></h4>
             <div className="grid">
             {bulbs.map(bulb => <Card {...bulb} />)}
             </div>
--- src/services/Bulbs.js
+++ src/services/Bulbs.js
@@ -12,18 +12,15 @@ export async function getBulbs() {
     }
 }
 export async function updateBulb(bulb_id, body) {
-    // body : {"is_on": true}
     const res = await axios.put(`http://group15.exceed19.online/bulb/${bulb_id}`, body)
     return res.data
 }
 
 export async function getParty() {
-    // body : {"is_on": true}
-    const res = await axios.put(`http://group15.exceed19.online/party`)
+    const res = await axios.get(`http://group15.exceed19.online/party`)
     return res.data
 }
 export async function updateParty(body) {
-    // body : {"is_on": true}
     const res = await axios.put(`http://group15.exceed19.online/party`, body)
     return res.data
 }
