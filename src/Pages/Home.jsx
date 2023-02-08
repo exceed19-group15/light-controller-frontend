3b1bc39e34d3b095baa416094a9b996c0dc9c3bc
breezjirasak <jirasak.tu@ku.th>
2023-02-08 16:11:11 +0700

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,32 +6,8 @@ import '../styles/Home.css'
 import { getBulbs } from '../services/Bulbs'
 
 
-const sampleData = [
-    {
-        bulb_id: 1, 
-        room_name: "Bedroom", 
-        is_on: true, 
-        is_auto: false, 
-        light_level: 50 
-    },
-    {
-        bulb_id: 2, 
-        room_name: "Bedroom", 
-        is_on: true, 
-        is_auto: false, 
-        light_level: 50 
-    },
-    {
-        bulb_id: 3, 
-        room_name: "Bedroom", 
-        is_on: true, 
-        is_auto: false, 
-        light_level: 50 
-    }
-]
-
 const Home = () => {
-    const [bulbs, setBulb] = useState(sampleData)
+    const [bulbs, setBulb] = useState([])
 
     useEffect(() => {
         getBulbs().then(data => setBulb(data))
@@ -39,8 +15,7 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            {/* <Card bulb_id={bulbs[0].bulb_id}/>
-            <Card bulb_id={bulbs.bulb_id}/> */}
+            <h1>Light Controller</h1>
             <div className="grid">
             {bulbs.map(bulb => <Card {...bulb} />)}
             </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
 import "../styles/Card.css";
-import ToggleSwitch from "./Button";
-import Slidebar from "./SlideBar";
+// import Slidebar from "./SlideBar";
 import React, { useState } from "react";
+import Slider from "@mui/material/Slider";
+import Switch from "@mui/material/Switch";
 
 const Card = ({ bulb_id, room_name, is_on, is_auto, light_level }) => {
   // const routeName = `/blub/${bulb_id}`
 
-  const [Switch, setSwitch] = useState(false);
+  const [switchState, setSwitchState] = useState(is_on);
+  const label = { inputProps: { "aria-label": "Switch demo" } };
 
   const onSwitchChange = (checked) => {
-    setSwitch(checked);
+    setSwitchState(checked);
     console.log(checked);
   };
-  const [Scale, setScale] = useState(0);
+  const [Scale, setScale] = useState(light_level);
+  console.log(Scale, light_level);
 
   const onScale = (event) => {
     setScale(event.target.value);
@@ -27,24 +30,14 @@ const Card = ({ bulb_id, room_name, is_on, is_auto, light_level }) => {
           <p>{room_name}</p>
         </div>
         <div>
-          <ToggleSwitch
-            id={"switch1"}
-            label="On/Off"
-            check={Switch}
-            onChange={onSwitchChange}
-          />
-
-          <label htmlFor="switch1">Manual</label>
-
-          <ToggleSwitch
-            id={"switch2"}
-            label="Manual"
-            check={Switch}
-            onChange={onSwitchChange}
-          />
-          
+          <Switch {...label} check={switchState} onChange={onSwitchChange} />
           <label htmlFor="switch2">On/Off</label>
-          <Slidebar id="slide" value={Scale} onChange={onScale} />
+          <Slider
+            aria-label="Default"
+            valueLabelDisplay="auto"
+            value={Scale}
+            onChange={onScale}
+          />
         </div>
       </div>
     </div>
