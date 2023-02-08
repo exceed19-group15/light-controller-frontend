f898021df063daa026fb43fa3f07b7f34de8e53a
breezjirasak <jirasak.tu@ku.th>
2023-02-08 13:02:36 +0700

    add card

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/Card.css'
+
+const Card = ({ bulb_id, room_name, is_on, is_auto, light_level  }) => {
+
+
+	// const routeName = `/blub/${bulb_id}`
+
+	return (
+		<div className='card'>
+            <p>dksal;dkas;ldkl;sadk;saldks;a;d;skads;a
+                dsmkaldlsadksadjs;ajdsadjslajdslajdlsa
+                djksaldjaklsjdlsajdlaskjdlsa
+                dsakjldjkasldjkasdjlkasddsa
+            </p>
+            <div>
+
+            <div>
+			
+            </div>
+
+			<div className="">
+                
+			</div>
+
+            </div>
+		</div>
+	)
+}
+
+export default Card
